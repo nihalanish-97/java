4157ddb13d83b6e5eb47de6d37043a30e333e282
nihal anish <dr.nihalanish@gmail.com>
2026-09-05 13:00:12 +0530

    class

--- int.java
+++ int.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class numrs {
+public class int{
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
-        int sum = 0;
+        int reverse = 0;
 
-        while (n != 0) {
+        while (n > 0) {
             int digit = n % 10;
-            sum = sum + digit;
+            reverse = reverse * 10 + digit;
             n = n / 10;
         }
 
-        System.out.println("Sum of digits = " + sum);
+        System.out.println("Reverse = " + reverse);
     }
+}}
 }
