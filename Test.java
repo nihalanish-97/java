a30a35612e4cce731fb6a7e22258c08a0aac392f
nihal anish <dr.nihalanish@gmail.com>
2026-09-25 13:02:14 +0530

    test

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test {
+
+    // Final variable
+    final int number = 10;
+
+    void show() {
+        System.out.println(number);
+    }
+
+    public static void main(String[] args) {
+
+        Test t = new Test();
+        t.show();
+    }
+}
