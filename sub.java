96c5685e6037054da03e3f97a7807e47e2aea227
nihal anish <dr.nihalanish@gmail.com>
2026-09-25 12:33:06 +0530

    sub

---
+++ sub.java
@@ -0,0 +1,21 @@
+public class sub {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {5, 6},
+            {7, 8}
+        };
+
+        int[][] b = {
+            {1, 2},
+            {3, 4}
+        };
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print((a[i][j] - b[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
