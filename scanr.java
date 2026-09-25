84cc43e2302f64dabfb71a2f3e1080354156617d
nihal anish <dr.nihalanish@gmail.com>
2026-09-25 12:40:12 +0530

    input

---
+++ scanr.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class scanr {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] a = new int[2][2];
+
+        System.out.println("Enter 4 numbers:");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix:");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
