fe95f904d29a36c752409898399eb3c880ed4de7
nihal anish <dr.nihalanish@gmail.com>
2026-09-25 12:38:19 +0530

    col

---
+++ col.java
@@ -0,0 +1,21 @@
+public class col {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for (int j = 0; j < 3; j++) {
+
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++) {
+                sum = sum + a[i][j];
+            }
+
+            System.out.println("Column " + (j + 1) + " sum = " + sum);
+        }
+    }
+}
