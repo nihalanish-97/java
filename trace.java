be511ccc451713ccd4460667c070b950ae9b360c
nihal anish <dr.nihalanish@gmail.com>
2026-09-25 12:35:44 +0530

    trace

---
+++ trace.java
@@ -0,0 +1,16 @@
+public class trace{
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 2; i++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
